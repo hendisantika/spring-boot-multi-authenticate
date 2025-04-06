b8173851510801d8d204b1a568f7db5757e04373
hendisantika <hendisantika@yahoo.co.id>
2025-04-06 07:06:46 +0700

    feat: Add authenticationProvider in SpringSecurityConfig🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/multiauthenticate/config/SpringSecurityConfig.java
+++ src/main/java/id/my/hendisantika/multiauthenticate/config/SpringSecurityConfig.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -70,6 +72,13 @@ public class SpringSecurityConfig {
         http.csrf(AbstractHttpConfigurer::disable);
 
         return http.build();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userDetailsService());
+        return authenticationProvider;
     }
 
 }
