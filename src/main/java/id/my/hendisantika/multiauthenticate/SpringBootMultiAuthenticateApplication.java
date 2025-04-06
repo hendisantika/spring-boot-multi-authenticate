585c193230053660fd6cce9c5c4a41b718a5e1da
hendisantika <hendisantika@yahoo.co.id>
2025-04-06 07:01:55 +0700

    feat: Init Project🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/multiauthenticate/SpringBootMultiAuthenticateApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.multiauthenticate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMultiAuthenticateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMultiAuthenticateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/hendisantika/multiauthenticate/SpringBootMultiAuthenticateApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.multiauthenticate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMultiAuthenticateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
