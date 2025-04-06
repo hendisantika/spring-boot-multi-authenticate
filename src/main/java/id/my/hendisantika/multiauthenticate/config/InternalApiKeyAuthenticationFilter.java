0f1e13dd236f47fd1c99c3bee802bcc4c66a3c2e
hendisantika <hendisantika@yahoo.co.id>
2025-04-06 07:05:35 +0700

    feat: Add InternalApiKeyAuthenticationFilter🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/multiauthenticate/config/InternalApiKeyAuthenticationFilter.java
@@ -0,0 +1,77 @@
+package id.my.hendisantika.multiauthenticate.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.FilterConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+
+import java.io.IOException;
+import java.util.Map;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : multi-authenticate
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 06/04/25
+ * Time: 07.04
+ * To change this template use File | Settings | File Templates.
+ */
+public class InternalApiKeyAuthenticationFilter implements Filter {
+
+    private final String internalApiKey;
+
+
+    InternalApiKeyAuthenticationFilter(String internalApiKey) {
+        this.internalApiKey = internalApiKey;
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+        HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+
+        String apiKey = httpServletRequest.getHeader("x-api-key");
+
+        if (apiKey == null) {
+            unauthorized(httpServletResponse);
+            return;
+        }
+
+        if (!internalApiKey.equals(apiKey)) {
+            unauthorized(httpServletResponse);
+            return;
+        }
+
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+
+    private void unauthorized(HttpServletResponse httpServletResponse) throws IOException {
+        httpServletResponse.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
+        httpServletResponse.setStatus(401);
+        Map<String, Object> response = Map.of("message", "SC_UNAUTHORIZED");
+        String responseBody = new ObjectMapper().writeValueAsString(response);
+        httpServletResponse.getWriter().write(responseBody);
+
+    }
+
+}
